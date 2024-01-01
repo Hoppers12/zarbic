8f90ee16f6d15e6d1835c6ff239da4f514deab9d
Hoppers12 <robin.ciurlik@gmail.com>
2024-01-01 13:01:44 +0100

    nn

--- Pile.py
+++ Pile.py
@@ -2,34 +2,31 @@
 qui permet de manipuler des piles/file
 '''
 import queue  # Import de la classe queue
-''' La 
-Classe qui permet de manipuler une pile
-'''
+ #Classe qui permet de manipuler une file
 class Pile:
-    '''Le
-        Constructeur de la classe '''
+    #Le constructeur de la pile
     def __init__(self):
         self.pile_colis = queue.LifoQueue()  # Création d'une pile
 
-    ''' la
-     Fonction qui teste si la pile est vide
-    '''
+    #Fonction qui teste si la pile est vide
     def empty(self):
-        '''Cette méthode retourne si la pile est vide ou non'''
+        """
+           Retourne si la pile est vide ou non.
+           Args : objet courant
+       """
         return self.pile_colis.empty()
-    ''' la
-    Fonction qui empile une valeur
-    '''
+       #La fonction qui empile une valeur
+
     def ajouter_valeur(self,valeur):
-        '''Cette fonction ajoute un valeur dans la pile
-        elle prend en paramètre la valeur a ajouter '''
+        """Cette fonction ajoute un valeur dans la pile
+        elle prend en paramètre la valeur a ajouter """
         return self.pile_colis.put(valeur)
 
     ''' la 
      Fonction qui dépile
     '''
     def depiler(self):
-        ''' Cette méthode dépile l'élément le plus haut de la pile'''
+        """ Cette méthode dépile l'élément le plus haut de la pile"""
         return self.pile_colis.get()
 
 
